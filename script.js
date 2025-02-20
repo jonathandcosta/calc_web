10f02aa7f69734af3c89089de7a8780faacaee04
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-20 12:01:54 -0300

    feat: add calculate result

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function appendOperator(operator) {
   currentInput += operator;
   display.textContent = currentInput;
 }
+
+// calulate
+function calculate() {
+  try {
+    let result = eval(currentInput);
+    if (!Number.isInteger(result)) {
+      result = result.toFixed(2);
+    }
+    currentInput = result;
+    display.textContent = currentInput;
+  } catch (error) {
+    display.textContent = 'erro';
+    currentInput = '';
+  }
+}
