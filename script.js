919724c960e6f2b4c704b2487f2870e87bfbb67f
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-20 11:51:36 -0300

    feat: show operator in display

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let display = document.getElementById('display');
 let currentInput = '';
 let currentOperator = '';
 
+// show numbers
 function appendNumber(value) {
   currentInput += value;
+  display.textContent = currentInput;
+}
+
+// show operator
+function appendOperator(operator) {
+  if (currentInput === '' && operator !== '.') return;
+  currentInput += operator;
   display.textContent = currentInput;
 }
